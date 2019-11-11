a301ac6a0f6204c3501d560fefa0fe5cf9fab5af
alan <alanyd.ph@gmail.com>
2019-11-11 11:18:34 -0300

    agregado de metodos en comentarios api

--- api/config/configApi.php
+++ api/config/configApi.php
@@ -3,6 +3,7 @@ class configApi
 {
     public static $RESOURCE = 'resource';
     public static $PARAMS = 'params';
+    public static $METHOD = 'method';
     public static $RESOURCES = [
       'detalles#GET'=> 'ComentariosApiController#mostrarComentario',
     ];
--- api/controller/ComentariosApiController.php
+++ api/controller/ComentariosApiController.php
@@ -1,15 +1,29 @@
 <?php
 require_once "Api.php";
+require_once "./../../model/ComentariosModel.php";
 class ComentariosApiController extends Api
 {
    private $model;
     function __construct()
     {
         parent::__construct();
-        $this->model = new TareasModel();
+        $this->model = new ComentariosModel();
     }
-    function GetComentarios(){
-        $data = $this->model->GetComentarios();
+
+    function GetComentarios($param = null){
+
+        if(isset($param)){
+            $id_comentario = $param[0];
+            $data = $this->model->GetComentarios($id_comentario);
+        }else{
+            $data = $this->model->GetComentarios();
+        }
+        if (isset($data)){
+            $this->json_response($data, 200);
+        }else{
+            $this->json_response(null, 404);
+        }
+
         //completar
     }
 
